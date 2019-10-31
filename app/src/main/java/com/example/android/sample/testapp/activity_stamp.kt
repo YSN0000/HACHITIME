1f68d44dd8426ba4571f82e9687d2d3d5a51f377
G016C1104 <tanasa759@yahoo.co.jp>
2019-10-31 14:30:25 +0900

    スタンプ作成

---
+++ app/src/main/java/com/example/android/sample/testapp/activity_stamp.kt
@@ -0,0 +1,12 @@
+package com.example.android.sample.testapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_stamp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stamp)
+    }
+}
